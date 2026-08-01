934c7b217b5dfb0ca2cf5ef68d7adfeed0ace87d
TTsdzb <ttsdzb@outlook.com>
2026-08-01 16:13:13 +0800

    feat(tauri): initialize Android APK project

---
+++ apps/tauri/src-tauri/gen/android/app/src/main/java/io/github/ttsdzb/metacubexd/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.ttsdzb.metacubexd
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
